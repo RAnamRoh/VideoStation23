cfad642b552f7bb78fa68c644f830e9f6f9053a3
Rakibul Anam <rakibul.anam@brainstation-23.com>
2024-08-29 14:49:44 +0600

    Initial Commit

---
+++ VideoStation23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoStation23
+//
+//  Created by BS00834 on 29/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoStation23/VideoStation23App.swift
@@ -0,0 +1,17 @@
+//
+//  VideoStation23App.swift
+//  VideoStation23
+//
+//  Created by BS00834 on 29/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoStation23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
